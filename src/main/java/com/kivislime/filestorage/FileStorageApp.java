959172c5b9a171e25c00a743a9c062a703ed0617
spinachh <kivislime@HypocritPc.>
2025-07-21 15:20:52 +0700

    style(app): add validation TODO and debug startup message

--- src/main/java/com/kivislime/filestorage/FileStorageApp.java
+++ src/main/java/com/kivislime/filestorage/FileStorageApp.java
@@ -2,11 +2,12 @@ package com.kivislime.filestorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//TODO: добавить валидацию starter-validation? Чтобы проверять контроллеры на входе
 @SpringBootApplication
 public class FileStorageApp {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Dikii");
 		SpringApplication.run(FileStorageApp.class, args);
 	}
 
