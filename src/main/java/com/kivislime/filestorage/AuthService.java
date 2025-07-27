fe8ca309b57e8edeb9873bca8f99263f412b407d
kivislime <kivislime@HypocritPc.>
2025-07-27 18:29:39 +0700

    refactor(auth): simplify AuthService removing unused dependencies

--- src/main/java/com/kivislime/filestorage/AuthService.java
+++ src/main/java/com/kivislime/filestorage/AuthService.java
@@ -2,23 +2,13 @@ package com.kivislime.filestorage;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-
 @RequiredArgsConstructor
 @Service
-public class AuthService  {
+public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
