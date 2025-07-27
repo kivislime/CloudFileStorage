8a96ea195ec6e17a4d40cce311e2ea783c1e6d51
kivislime <kivislime@HypocritPc.>
2025-07-27 18:29:39 +0700

    feat(auth): update AuthController for JSON-based sign-in and sign-up endpoints

--- src/main/java/com/kivislime/filestorage/AuthController.java
+++ src/main/java/com/kivislime/filestorage/AuthController.java
@@ -4,33 +4,26 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserService userService;
+    private final AuthService userService;
 
-    @PostMapping("sign-in")
-    public ResponseEntity<AuthResponse> signIn(@RequestBody UserCredentialsDto userCredentialsDto) {
-        AuthResponse authResponse = userService.login(userCredentialsDto);
-        return new ResponseEntity<>(authResponse, HttpStatus.OK);
-    }
-
-    @PostMapping("sign-up")
+    @PostMapping("/sign-up")
     public ResponseEntity<AuthResponse> signUp(@RequestBody UserCredentialsDto userCredentialsDto) {
         AuthResponse authResponse = userService.register(userCredentialsDto);
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
-        //        return ResponseEntity.status(HttpStatus.CREATED).body(authResponse);
     }
 
 //    @PostMapping("sign-out")
 //    public ResponseEntity<AuthResponse> signOut() {
-//        //TODO: из куки достать? юзера
+//        //TODO: из куки достать? юзера. UPD: через параметр метода контроллера положить UsernamePasswordAuthenticationToken. Так как он расширяет Authentication
 //    }
+
+    //TODO: надо куда-то положить этот эндпоинт тоже, отдельным сделать? RequestMapping мешают положить в другие
+    // @GetMapping("user/me")
 }
-//TODO: РЕАЛИЗОВАТЬ методы post. get - не нужны так как будут лежать на фронте. Пишу без jwt(мб потом написать версию с jwt, прописаьт в задачи, перейти вроде не сложно)
+//TODO: Протестироваьт вход логин, написать тест? Пишу без jwt(мб потом написать версию с jwt, прописаьт в задачи, перейти вроде не сложно)
