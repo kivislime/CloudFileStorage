6397ca7839ea4dc08a10976da23770831070e98b
kivislime <kivislime@HypocritPc.>
2025-08-04 13:00:00 +0700

    feat(api): update FileController to support streaming download, move, rename and delete operations

--- src/main/java/com/kivislime/filestorage/FileController.java
+++ src/main/java/com/kivislime/filestorage/FileController.java
@@ -1,7 +1,10 @@
 package com.kivislime.filestorage;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +29,36 @@ public class FileController {
     @GetMapping("/search")
     public ResponseEntity<List<FileInfoDto>> searchFiles(@RequestParam String path,
                                                          @AuthenticationPrincipal UserPrincipal principal) {
-        List<FileInfoDto> fileList = fileService.getFileList(path, principal.getId());
+        List<FileInfoDto> fileList = fileService.getFileList(principal.getId(), path);
         return new ResponseEntity<>(fileList, HttpStatus.OK);
+    }
+
+    @GetMapping("/move")
+    public ResponseEntity<FileInfoDto> moveFile(@RequestParam String fromKey,
+                                                @RequestParam String toKey,
+                                                @AuthenticationPrincipal UserPrincipal principal) {
+        FileInfoDto fileList;
+        if (FileParserUtil.getNameFromPath(fromKey).equals(FileParserUtil.getNameFromPath(toKey))) {
+            fileList = fileService.moveFile(principal.getId(), fromKey, toKey);
+        } else {
+            fileList = fileService.renameFile(principal.getId(), fromKey, toKey);
+        }
+
+        return new ResponseEntity<>(fileList, HttpStatus.OK);
+    }
+
+    @GetMapping("download")
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam String path,
+                                                            @AuthenticationPrincipal UserPrincipal principal) {
+        //TODO: вернуться к этой идее после того как подниму фронтенд
+        // PresignedUrlDto url = fileService.downloadFileByUrl(principal.getId(), path);
+        // return new ResponseEntity<>(url, HttpStatus.OK);
+        FileDownloadDto file = fileService.download(principal.getId(), path);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"archive.zip\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(file.contentLength())
+                .body(file.stream());
     }
 
     //TODO: какой будет exception при файлу размером больше 10 кб? чек
@@ -38,9 +69,20 @@ public class FileController {
     public ResponseEntity<List<FileInfoDto>> uploadFile(@RequestParam String path,
                                                         @RequestParam MultipartFile file,
                                                         @AuthenticationPrincipal UserPrincipal principal) throws IOException {
-        FileUploadRequest fileUploadRequest = new FileUploadRequest(file.getOriginalFilename(), file.getContentType(), file.getSize(), file.getBytes()); //TODO: маппер?
+        FileUploadRequest fileUploadRequest = new FileUploadRequest(
+                file.getOriginalFilename(),
+                file.getContentType(),
+                file.getSize(),
+                file.getBytes()); //TODO: маппер?
         List<FileInfoDto> fileInfoDtoList = fileService.uploadFile(principal.getId(), path, fileUploadRequest);
         return new ResponseEntity<>(fileInfoDtoList, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteFile(@RequestParam String path,
+                                           @AuthenticationPrincipal UserPrincipal principal) {
+        fileService.deleteFile(principal.getId(), path);
+        return ResponseEntity.noContent().build();
     }
 
 }
