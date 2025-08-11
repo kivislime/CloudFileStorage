7f798dc103c7156657187338d0f9270b4a22c9d5
kivislime <kivislime@HypocritPc.>
2025-08-11 13:37:28 +0700

    feat(repo): introduce FileRepositoryFacade for transactional file operations

---
+++ src/main/java/com/kivislime/filestorage/FileRepositoryFacade.java
@@ -0,0 +1,57 @@
+package com.kivislime.filestorage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class FileRepositoryFacade {
+    private final FileRepository fileRepository;
+
+    public UserFile findByUserIdAndObjectKey(Long userId, String path) {
+        return fileRepository.findByUserIdAndObjectKey(userId, path)
+                .orElseThrow(() -> new RuntimeException("User with id: " + userId + " not found"));
+    }
+
+
+    public List<UserFile> findByUserIdAndObjectKeyStartingWith(Long userId, String path) {
+        return fileRepository.findByUserIdAndObjectKeyStartingWith(userId, path);
+    }
+
+    @Transactional
+    public UserFile saveFile(Long userId, String objectKey, Long size, StorageItemType type) {
+        UserFile newFile = new UserFile();
+        newFile.setUserId(userId);
+        newFile.setObjectKey(objectKey);
+        newFile.setSize(size);
+        newFile.setStorageItemType(type);
+
+        try {
+            return fileRepository.save(newFile);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("File already exists", e);
+        }
+    }
+
+    @Transactional
+    public UserFile updateFile(Long userId, String fromKey, String toKey) {
+        UserFile userFile = fileRepository.findByUserIdAndObjectKey(userId, fromKey)
+                .orElseThrow(() -> new RuntimeException("User with id: " + userId + " not found"));
+
+        userFile.setObjectKey(toKey);
+        try {
+            return fileRepository.save(userFile);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("File already exists", e);
+        }
+    }
+
+    @Transactional
+    public void deleteByUserIdAndObjectKey(Long userId, String path) {
+        fileRepository.deleteByUserIdAndObjectKey(userId, path);
+    }
+}
