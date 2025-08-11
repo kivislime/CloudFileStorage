2fcd263bf6264c305b5b933e2f98b98eabeb1fb3
kivislime <kivislime@HypocritPc.>
2025-08-11 13:37:28 +0700

    feat(directory): add DirectoryService for listing and creating directories

---
+++ src/main/java/com/kivislime/filestorage/DirectoryService.java
@@ -0,0 +1,56 @@
+package com.kivislime.filestorage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class DirectoryService {
+    private final FileRepository fileRepository;
+    private final FileInfoMapper fileInfoMapper;
+
+    public List<FileInfoDto> listDirectChildren(Long userId, String path) {
+        List<UserFile> fileList = fileRepository.findDirectChildren(userId, path);
+        return fileInfoMapper.toDtoList(fileList);
+    }
+
+    @Transactional
+    public List<FileInfoDto> createMissingDirectoriesForPath(String directoryPath, Long userId) {
+        List<String> prefixes = ResourceParserUtil.buildPrefixes(directoryPath);
+        List<UserFile> existing = fileRepository.findByUserIdAndObjectKeyInAndStorageItemType(
+                userId,
+                prefixes,
+                StorageItemType.DIRECTORY);
+
+        Set<String> existingKeys = existing.stream()
+                .map(UserFile::getObjectKey)
+                .collect(Collectors.toSet());
+
+        List<UserFile> toCreate = prefixes.stream()
+                .filter(s -> !existingKeys.contains(s))
+                .map(s -> {
+                    UserFile uf = new UserFile();
+                    uf.setUserId(userId);
+                    uf.setObjectKey(s);
+                    uf.setStorageItemType(StorageItemType.DIRECTORY);
+                    return uf;
+                })
+                .toList();
+
+        if (!toCreate.isEmpty()) {
+            fileRepository.saveAll(toCreate);
+        }
+
+        List<UserFile> result = new ArrayList<>(existing.size() + toCreate.size() + 1);
+        result.addAll(existing);
+        result.addAll(toCreate);
+
+        return fileInfoMapper.toDtoList(result);
+    }
+}
