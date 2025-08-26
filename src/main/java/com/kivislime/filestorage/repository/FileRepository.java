9c3abf774e6266cedc3432730647975bb6d1927e
kivislime <kivislime@HypocritPc.>
2025-08-26 16:19:36 +0700

    fix(repository): add repository methods and improve exception handling

--- src/main/java/com/kivislime/filestorage/repository/FileRepository.java
+++ src/main/java/com/kivislime/filestorage/repository/FileRepository.java
@@ -3,6 +3,7 @@ package com.kivislime.filestorage.repository;
 import com.kivislime.filestorage.entity.StorageItemType;
 import com.kivislime.filestorage.entity.UserFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -18,20 +19,29 @@ public interface FileRepository extends JpaRepository<UserFile, Long> {
     List<UserFile> findByUserIdAndObjectKeyInAndStorageItemType(Long userId, List<String> path, StorageItemType storageItemType);
 
     @Query("""
-             SELECT uf
-               FROM UserFile uf
-              WHERE uf.userId = :userId
-                AND (
-                  (uf.objectKey LIKE CONCAT(:path, '%')
-                   AND uf.objectKey NOT LIKE CONCAT(:path, '%', '/', '%')
-                  )
-                  OR
-                  (uf.objectKey LIKE CONCAT(:path, '%/')
-                   AND uf.objectKey NOT LIKE CONCAT(:path, '%/%', '/', '%')
-                  )
+            SELECT uf
+            FROM UserFile uf
+            WHERE uf.userId = :userId
+              AND (
+                (uf.objectKey LIKE CONCAT(:path, '%')
+                 AND uf.objectKey NOT LIKE CONCAT(:path, '%', '/', '%')
                 )
+                OR
+                (uf.objectKey LIKE CONCAT(:path, '%/')
+                 AND uf.objectKey NOT LIKE CONCAT(:path, '%/%', '/', '%')
+                )
+              )
             """)
     List<UserFile> findDirectChildren(Long userId, String path);
 
-    void deleteByUserIdAndObjectKey(Long userId, String path);
+    @Modifying(clearAutomatically = true)
+    int deleteByUserIdAndObjectKeyStartingWith(Long userId, String prefix);
+
+    boolean existsByUserIdAndObjectKey(Long userId, String objectKey);
+
+    List<UserFile> findAllByUserIdAndObjectKeyIn(Long userId, List<String> objectKey);
+
+    boolean existsByUserIdAndObjectKeyAndStorageItemType(Long userId, String path, StorageItemType storageItemType);
+
+    List<UserFile> findByUserIdAndStorageItemTypeAndObjectKeyStartingWith(Long userId, StorageItemType type, String fromKey);
 }
--- src/main/java/com/kivislime/filestorage/repository/FileRepositoryFacade.java
+++ src/main/java/com/kivislime/filestorage/repository/FileRepositoryFacade.java
@@ -3,13 +3,14 @@ package com.kivislime.filestorage.repository;
 import com.kivislime.filestorage.entity.StorageItemType;
 import com.kivislime.filestorage.entity.UserFile;
 import com.kivislime.filestorage.exception.FileAlreadyExistsException;
-import com.kivislime.filestorage.exception.UserNotFoundException;
+import com.kivislime.filestorage.exception.UserFileNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
@@ -18,7 +19,7 @@ public class FileRepositoryFacade {
 
     public UserFile findByUserIdAndObjectKey(Long userId, String path) {
         return fileRepository.findByUserIdAndObjectKey(userId, path)
-                .orElseThrow(() -> new UserNotFoundException("User with id: " + userId + " not found"));
+                .orElseThrow(() -> new UserFileNotFoundException("File for user with id: " + userId + " not found, by path: " + path));
     }
 
     public List<UserFile> findByUserIdAndObjectKeyStartingWith(Long userId, String path) {
@@ -36,25 +37,59 @@ public class FileRepositoryFacade {
         try {
             return fileRepository.save(newFile);
         } catch (DataIntegrityViolationException e) {
-            throw new FileAlreadyExistsException("File already exists", e);
+            throw new FileAlreadyExistsException("File for user with id: " + userId + " already exists, by path: " + objectKey, e);
         }
     }
 
     @Transactional
     public UserFile updateFile(Long userId, String fromKey, String toKey) {
         UserFile userFile = fileRepository.findByUserIdAndObjectKey(userId, fromKey)
-                .orElseThrow(() -> new UserNotFoundException("User with id: " + userId + " not found"));
+                .orElseThrow(() -> new UserFileNotFoundException("File for user with id: " + userId + " not found, by path: " + fromKey));
 
         userFile.setObjectKey(toKey);
         try {
-            return fileRepository.save(userFile);
+            return fileRepository.saveAndFlush(userFile);
         } catch (DataIntegrityViolationException e) {
-            throw new FileAlreadyExistsException("File already exists", e);
+            throw new FileAlreadyExistsException("File for user with id: " + userId + " already exists, by path: " + toKey, e);
         }
     }
 
     @Transactional
-    public void deleteByUserIdAndObjectKey(Long userId, String path) {
-        fileRepository.deleteByUserIdAndObjectKey(userId, path);
+    public void saveAll(List<UserFile> files) {
+        try {
+            fileRepository.saveAll(files);
+            fileRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            Long userId = files.isEmpty() ? null : files.get(0).getUserId();
+            String objectKeys = files.stream()
+                    .map(UserFile::getObjectKey)
+                    .collect(Collectors.joining(", "));
+
+            throw new FileAlreadyExistsException("File(s) already exist for userId=" + userId + ", objectKeys=[" + objectKeys + "]", e);
+        }
     }
+
+
+    @Transactional
+    public void deleteFile(UserFile userFile) {
+        fileRepository.delete(userFile);
+    }
+
+    @Transactional
+    public int deleteAllByUserIdAndObjectKey(Long userId, String objectKey) {
+        return fileRepository.deleteByUserIdAndObjectKeyStartingWith(userId, objectKey);
+    }
+
+    public List<UserFile> findAllByUserIdAndObjectKeyIn(Long userId, List<String> objectKey) {
+        return fileRepository.findAllByUserIdAndObjectKeyIn(userId, objectKey);
+    }
+
+    public List<UserFile> findByUserIdAndStorageItemTypeAndObjectKeyStartingWith(Long userId, StorageItemType type, String fromKey) {
+        return fileRepository.findByUserIdAndStorageItemTypeAndObjectKeyStartingWith(userId, type, fromKey);
+    }
+
+    public boolean userFileExists(Long userId, String toKey) {
+        return fileRepository.existsByUserIdAndObjectKey(userId, toKey);
+    }
+
 }
