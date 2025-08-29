b9e8fd354fe83e4e8a15c4d9acea82e75c3bc80b
kivislime <kivislime@HypocritPc.>
2025-08-29 12:37:17 +0700

    fix(exception): add missing constructor to ObjectStorageException

--- src/main/java/com/kivislime/filestorage/exception/ObjectStorageException.java
+++ src/main/java/com/kivislime/filestorage/exception/ObjectStorageException.java
@@ -1,6 +1,9 @@
 package com.kivislime.filestorage.exception;
 
 public class ObjectStorageException extends RuntimeException {
+    public ObjectStorageException(String s) {
+        super(s);
+    }
     public ObjectStorageException(String s, Throwable e) {
         super(s, e);
     }
