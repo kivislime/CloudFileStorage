d0e95e2f6b3e3ba59026a57b3ad75ef66242b3e5
kivislime <kivislime@HypocritPc.>
2025-08-30 13:52:45 +0700

    deploy

--- src/main/java/com/kivislime/filestorage/config/CorsConfig.java
+++ src/main/java/com/kivislime/filestorage/config/CorsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -22,7 +23,13 @@ public class CorsConfig {
                 "http://77.110.104.170"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(Arrays.asList(
+                "Authorization",
+                "Set-Cookie",
+                "Access-Control-Allow-Origin"
+        ));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
